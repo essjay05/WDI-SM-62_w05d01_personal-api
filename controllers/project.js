45ea2f03a26c2daedb80ffb115c912f1a7629bf6
Joy <stepjoyous@gmail.com>
2018-12-11 10:22:44 -0800

    All CRUD works properly and master also now contains public folder

--- controllers/project.js
+++ controllers/project.js
@@ -4,46 +4,12 @@ const Project = require('../models/project');
 // CREATE CONRTOLS/JAVASCRIPT FUNCTIONS FOR CRUD:
 
 module.exports = {
-    // index: (req, res) => {
-    //     console.log(req.query);
-    //     Project.find({}, (err, projects) => {
-    //         if (err) res.json({ success: false, err });
-    //         projects.forEach( p => {
-    //             console.log(p.name);
-    //             if (req.query.name) {
-    //                 if (req.query.name.toLowerCase() == p.name.toLowerCase()) {
-    //                     console.log("Hi")
-    //                 }
-    //             }
-    //         })
-    //         res.json({ success: true, projects });
-    //     }).limit(parseInt(req.query))
-    // },
-
     index: (req, res) => {
-        
-        console.log(req.query);
-
         Project.find({}, (err, projects) => {
             if (err) res.json({ success: false, err });
-            projects.forEach(p => {
-            
-                console.log(p.name);
-                if (req.query.name) {
-                    if (req.query.name.toLowerCase() == p.name.toLowerCase()) {
-                        console.log("hi")
-                        res.json({ success: true, p })
-                    }
-                    else {
-                        res.json({ success: true, projects });
-                    }
-
-                } else {
-                    res.json({ success: true, projects });        
-                }
-            });
             res.json({ success: true, projects });
-        }).limit(parseInt(req.query.limit));
+        })
+    },
 
     show: (req, res) => {
         Project.findById(req.params.id, (err, project) => {
@@ -74,13 +40,13 @@ module.exports = {
         })
     },
 
-    // CRUD STRETCH GOALS:
-        // LIMIT PROJECT SEARCH TO TWO:
-    index2: (req, res) => {
-        Project.find({}, (err, projects) => {
-            if (err) res.json({ success: false, err });
-            res.json({ success: true, projects });
-        }).limit(2)
-    },
+    // // CRUD STRETCH GOALS:
+    //     // LIMIT PROJECT SEARCH TO TWO:
+    // index2: (req, res) => {
+    //     Project.find({}, (err, projects) => {
+    //         if (err) res.json({ success: false, err });
+    //         res.json({ success: true, projects });
+    //     }).limit(2)
+    // },
 
 }
